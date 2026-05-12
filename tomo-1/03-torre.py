5d52d25f881935b1e477bc109b7acf6e1e4d7149
Andryo <andryo.lemes28@gmail.com>
2026-05-12 18:49:40 -0300

    ajuste no exercício 03

--- tomo-1/03-torre.py
+++ tomo-1/03-torre.py
@@ -1,3 +1,7 @@
 Crystarium acima
 Crystal Tower abaixo
 Norvrandt entre as duas
+
+print("Crystarium acima", "Crystal Tower abaixo", "Norvrandt entre as duas")
+
+# Apenas demonstrando a função print() com múltiplos argumentos, separados por vírgula.
